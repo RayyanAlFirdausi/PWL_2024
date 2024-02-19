6239bf635c5194d0432ab6d3cdd92705373d869a
RayyanAlFirdausi <rayyanalfirdausy@gmail.com>
2024-02-19 19:49:03 +0700

    Modifikasi tampilan awal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -130,4 +130,19 @@
             </div>
         </div>
     </body>
+</html> --}}
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PWL 2024</title>
+</head>
+<body>
+    <h2>Pemograman web lanut 2024</h2>
+    <h3>Semangat belajar laravel 10</h3>
+    
+</body>
 </html>
